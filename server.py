b7cf2db5dc4b4a29f1d8309df2f2e016dfce5654
moritzkoster <moritz.koster@bluewin.ch>
2021-04-07 21:57:27 +0200

    new data structure, image requests are better handled, answers for bad requests

--- server.py
+++ server.py
@@ -1,14 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 import json
+import os
 
 app = Flask(__name__, static_url_path="/static")
 
 @app.route("/activities/<grade>", methods = ["GET"])
 def activities(grade):
-    with open("activities/" + grade + ".json") as file:
-        data = json.load(file)
-        return json.dumps(data)
+    if os.path.isfile("data/activities/" + grade + ".json"):                    #test for file
+        with open("data/activities/" + grade + ".json") as file:
+            data = json.load(file)
+            return json.dumps(data)
+    else:
+        return "Invalid Request: grade <b>" + grade + "</b> does not exist. duu Arsch" #BAD REQUEST
+
+@app.route('/images/<name>', methods = ["GET"])
+def images(name):
+    if os.path.isfile("data/images/" + name):                                   #test for file
+        return send_from_directory("data/images", name)
+    else:
+        return "Invalid Request: file <b>" + name + "</b> does not exist. duu Arsch" #BAD REQUEST
+
+
+@app.route("/<request>")
+def bad_request(request):
+    return "Invalid Request: <b>" + request + "</b> does not exist. duu Arsch"  #BAD REQUEST
 
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", port="8081")
+  app.run(host="0.0.0.0", port="8081", threaded=False)
